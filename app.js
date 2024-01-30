dd48f8d403d84441bdae3a7472ad87dbe22344a6
tanto <tantows001@gmail.com>
2024-01-30 14:24:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const server = app.listen(PORT, ()=>console.log(`server running on http://localhost:${PORT}`))
+
+app.use(express.static(path.join(__dirname, 'public')))
