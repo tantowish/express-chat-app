9c5837a482e855670393ec8b3479da02ccae9b28
tanto <tantows001@gmail.com>
2024-02-03 14:58:13 +0700

    final commit

--- app.js
+++ app.js
@@ -3,12 +3,46 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Run server
 const server = app.listen(PORT, ()=>console.log(`server running on http://localhost:${PORT}`))
 
+// Call socket
 const io = require('socket.io')(server)
 
+// make static public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', (socket)=>{
-    console.log(socket.id)
-})
+// Socket id list of set
+let socketsConnected = new Set()
+
+// Connecting to socket.io
+io.on('connection', onConnected)
+
+// Connected function
+function onConnected(socket){
+    // Connected log
+    console.log("new socket connected : "+socket.id)
+    socketsConnected.add(socket.id)
+
+    // Emitting the data to FE
+    io.emit('clients-total', socketsConnected.size)
+
+    // Disconnected log
+    socket.on('disconnect',()=>{
+        console.log("socket disconnected", socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+
+    // Handle emitted message
+    socket.on('message', (data)=>{
+        console.log(data)
+
+        // Emitting to all client
+        socket.broadcast.emit('chat-message', data)
+    })
+
+    socket.on('feedback',(data)=>{
+        socket.broadcast.emit('feedback', data)
+    })
+}
--- public/main.js
+++ public/main.js
@@ -1,1 +1,113 @@
+// Get sokcet
 const socket = io()
+
+// Get all required element
+const total = document.getElementById('client-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+// Message tone
+const messageTone = new Audio('/message-tone.mp3')
+
+// Messageform event when submit
+messageForm.addEventListener('submit', (e)=>{
+    if(messageInput.value === '') return
+    e.preventDefault()
+    sendMessage()
+})
+
+// Handle the emitted clients-total from BE
+socket.on('clients-total', (data)=>{
+    total.innerText = data
+})
+
+// Handle the send message to BE
+const sendMessage = ()=>{
+    console.log(messageInput.value)
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTIme: new Date()
+    }
+
+    // Emitting message to BE
+    socket.emit('message', data)
+    addMessage(true, data)
+
+    messageInput.value = ''
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+// Handle broadcasted chat
+socket.on('chat-message', (data)=>{
+    messageTone.play()
+    console.log(data)
+    addMessage(false, data)
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+})
+
+const addMessage = (isOwnMessage, data)=>{
+    clearFeedback()
+    const element = `
+        <div class="mb-2 ${isOwnMessage? 'text-right':'text-left'}">
+            <p class="${isOwnMessage? 'bg-blue-500 text-white': 'bg-gray-200 text-gray-700'} rounded-lg py-2 px-4 inline-block">${data.message}</p>
+            <p class="text-xs text-gray-700">${data.name} ${formatTimeDifference(data.dateTIme)}</p>
+        </div>
+    `
+    messageContainer.innerHTML += element
+}
+
+messageInput.addEventListener('focus', (e)=>{
+    socket.emit('feedback', {
+        feedback: `${nameInput.value} is typing a message`
+    })
+})
+
+messageInput.addEventListener('keypress', (e)=>{
+    socket.emit('feedback', {
+        feedback: `${nameInput.value} is typing a message`
+    })
+})
+
+messageInput.addEventListener('blur', () => {
+    socket.emit('feedback', {
+        feedback: ``
+    })
+});
+
+socket.on('feedback', (data)=>{
+    clearFeedback()
+    const element = `
+    <div class="mt-4 mb-2 text-center" id="feedback">
+        <p>${data.feedback}</p>
+    </div>`
+
+    messageContainer.innerHTML += element
+})
+
+const clearFeedback = ()=>{
+    document.querySelectorAll('#feedback').forEach(element=>{
+        element.parentNode.removeChild(element)
+    })
+}
+
+// Function to format the time difference
+const formatTimeDifference = (timestamp) => {
+    const now = new Date();
+    const secondsAgo = Math.floor((now - timestamp) / 1000);
+
+    if (secondsAgo < 60) {
+        return `${secondsAgo} second${secondsAgo !== 1 ? 's' : ''} ago`;
+    } else if (secondsAgo < 3600) {
+        const minutesAgo = Math.floor(secondsAgo / 60);
+        return `${minutesAgo} minute${minutesAgo !== 1 ? 's' : ''} ago`;
+    } else if (secondsAgo < 86400) {
+        const hoursAgo = Math.floor(secondsAgo / 3600);
+        return `${hoursAgo} hour${hoursAgo !== 1 ? 's' : ''} ago`;
+    } else {
+        const daysAgo = Math.floor(secondsAgo / 86400);
+        return `${daysAgo} day${daysAgo !== 1 ? 's' : ''} ago`;
+    }
+};
