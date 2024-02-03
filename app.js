0551df800fa7c3e7940271e4c25ad30ebf566e0d
tanto <tantows001@gmail.com>
2024-02-03 10:41:13 +0700

    feat: UI

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, ()=>console.log(`server running on http://localhost:${PORT}`))
 
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket)=>{
+    console.log(socket.id)
+})
